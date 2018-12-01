1e3f7cc31da6d1337f523760d095325a88b9d6a9
pravin <pravin@risksciences.ucla.edu>
2018-12-01 03:16:57 -0800

    wait

--- Commands.py
+++ Commands.py
@@ -26,3 +26,8 @@ class Robot:
 	# write a move command to serial
 	def move(self, dx, dy):	
 		self.ser.write(bytes('<'+str(self.rID)+','+str(dx)+','+str(dy)+str(self.theta)'>\n','utf-8'))
+
+	# wait until a done signal is received, update position
+	def wait(self):
+		ser.read_until('<done>')
+		self.get_position()
