490801f99ba66c245a5f27834164d4d5f7221bc3
pravin <pravin@risksciences.ucla.edu>
2018-11-30 23:29:52 -0800

    Robot Skeleton

---
+++ Commands.py
@@ -0,0 +1,18 @@
+import serial
+
+class Robot:
+
+	# serial port communication
+	ser = serial.Serial('/dev/ttyS0');
+	
+	# colors available - three available, must be unique for every instance
+	types = ['red', 'green', 'blue']
+
+	def __init__(self, rID, xInit, yInit):
+		self.rID = rID
+		self.lastX = xInit
+		self.lastY = yInit
+		self.type = self.types.pop()
+
+	# write a move command to serial
+	#def move(self, dx, dy):	
