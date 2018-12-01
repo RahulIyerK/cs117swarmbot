6dd7d2bb7fb87f406b916a1bcdf3ad5b327d842d
Joshua Lo <lo.joshua@outlook.com>
2018-12-01 01:31:06 -0800

    Initial Calibration and Movement

--- Robot Firmware/robot_calibration/robot_calibration.ino
+++ Robot Firmware/robot_calibration/robot_calibration.ino
@@ -1,5 +1,5 @@
 #include <cs117_swarmbot_library.h>
-
+#define 
 void setup() {
   // put your setup code here, to run once:
   pinMode(MOTOR_L_DIR_PIN, OUTPUT);
@@ -15,6 +15,24 @@ void setup() {
 
   digitalWrite(MOTOR_L_DIR_PIN, FORWARD);
   digitalWrite(MOTOR_R_DIR_PIN, FORWARD);
+}
+
+//The idea is to move forward for a set time and then based on the amount that's reported as moved,
+//we normalize the speed terms
+void forward_cal(){
+  digitalWrite(MOTOR_L_DIR_PIN, FORWARD);
+  digitalWrite(MOTOR_R_DIR_PIN, FORWARD);
+
+  analogWrite(MOTOR_L_PWM_PIN, 65);
+  analogWrite(MOTOR_R_PWM_PIN, 65);
+
+  delay(del);
+
+  //not sure how to make it stop so i'm just setitng the pins to 0
+
+  analogWrite(MOTOR_L_PWM_PIN, 0);
+  analogWrite(MOTOR_R_PWM_PIN, 0);
+
 }
 
 void loop() {
