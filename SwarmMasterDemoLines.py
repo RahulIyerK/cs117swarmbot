4f7fdf33fbe3ca5b2dd513a4005ccd930f4a0934
Derek Xu <derekqxu@gmail.com>
2018-12-03 00:39:13 -0800

    Update SwarmMasterDemoLines.py

--- SwarmMasterDemoLines.py
+++ SwarmMasterDemoLines.py
@@ -1,5 +1,6 @@
 from Commands import Robot
 from CV_script_with_classes import CV_input
+import cv2
 
 def main():
 	
@@ -15,13 +16,15 @@ def main():
 	r3 = Robot('qianru',2,0,0, camera)
 
 	# run command loop
+	while camera.initialized != 1:
+		camera.update()
+		intrrpt = cv2.waitKey(30) & 0xff
+		if intrrpt == 27:
+			camera.delet()
+			break
 	while True:
 		# update positions
 		camera.update()
-		
-		# find all initial positions
-		while(not (camera.initialized == 1)):
-			camera.update()
 
 		r1.move(1,0,0)
 		r2.move(1,0,0)
@@ -31,6 +34,10 @@ def main():
 		r1.wait()
 		#r2.wait()
 		#r3.wait()
+		intrrpt = cv2.waitKey(30) & 0xff
+		if intrrpt == 27:
+			camera.delet()
+			break
 
 if __name__ == "__main__":
 	main()
