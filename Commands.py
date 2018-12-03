da1c59550aacca9e86c00d0b11db9b5d96cb7b22
pravin <pravin@risksciences.ucla.edu>
2018-12-02 21:50:21 -0800

    removed line break

--- Commands.py
+++ Commands.py
@@ -25,7 +25,7 @@ class Robot:
 	
 	# write a move command to serial
 	def move(self, dx, dy):	
-		self.ser.write(bytes('< '+str(self.rID)+' '+str(dx)+' '+str(dy)+str(self.theta)+' >\n','utf-8'))
+		self.ser.write(bytes('< '+str(self.rID)+' '+str(dx)+' '+str(dy)+str(self.theta)+' >','utf-8'))
 
 	# wait until a done signal is received, update position
 	def wait(self):
